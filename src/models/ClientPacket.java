c1c3ab5409f514c1d30c9aa175e8078eb29ff1d2
Quinn Pollock <contact@quinnpollock.net>
2019-11-14 13:54:55 -0500

    Utils Refactor

--- src/models/ClientPacket.java
+++ src/models/ClientPacket.java
@@ -1,6 +1,6 @@
 package models;
 
-public class DNSPacket {
+public class ClientPacket {
     String gatewayMac;
     String mac;
     String siteIp;
@@ -9,7 +9,7 @@ public class DNSPacket {
     int srcPort;
     String appData;
 
-    public DNSPacket(String siteIp, String ip, int srcPort, String appData) {
+    public ClientPacket(String siteIp, String ip, int srcPort, String appData) {
         this.gatewayMac = "00:1b:21:36:61:13";
         this.mac = "78:4f:43:55:b1:69";
         this.siteIp = siteIp;
@@ -17,5 +17,18 @@ public class DNSPacket {
         this.dstPort = 80;
         this.srcPort = srcPort;
         this.appData = appData;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientPacket{\n" +
+                "gatewayMac='" + gatewayMac + '\'' +
+                ",\n mac='" + mac + '\'' +
+                ",\n siteIp='" + siteIp + '\'' +
+                ",\n Ip='" + Ip + '\'' +
+                ",\n dstPort=" + dstPort +
+                ",\n srcPort=" + srcPort +
+                ",\n appData='" + appData + '\'' + "\n" +
+                '}';
     }
 }

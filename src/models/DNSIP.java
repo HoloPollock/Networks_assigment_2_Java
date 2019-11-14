55a49e94b206177862f25d97fcf0eb79f53951bb
Quinn Pollock <contact@quinnpollock.net>
2019-11-14 13:54:46 -0500

    Utils Refactor

--- src/models/DNSIP.java
+++ src/models/DNSIP.java
@@ -4,17 +4,25 @@ import inet.ipaddr.ipv4.IPv4Address;
 import inet.ipaddr.ipv6.IPv6Address;
 
 public class DNSIP {
-    String IPv4;
-    String IPv6;
+    private String IPv4;
+    private String IPv6;
 
-   public DNSIP(IPv4Address iPv4, IPv6Address iPv6) {
-       this.IPv6 = iPv6.toString();
-       this.IPv4 = iPv4.toString();
-   }
+    public DNSIP(IPv4Address iPv4, IPv6Address iPv6) {
+        this.IPv6 = iPv6.toString();
+        this.IPv4 = iPv4.toString();
+    }
 
     public DNSIP(String IPv4, String IPv6) {
         this.IPv4 = IPv4;
         this.IPv6 = IPv6;
+    }
+
+    public String getIPv4() {
+        return IPv4;
+    }
+
+    public String getIPv6() {
+        return IPv6;
     }
 
     @Override
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,9 +1,10 @@
 package utils;
+
 public class Utils {
-    public static String responseToString(byte[] arr){
+    public static String responseToString(byte[] arr) {
         StringBuilder response = new StringBuilder();
         for (var b : arr) {
-            if(b != 0) {
+            if (b != 0) {
                 response.append((char) b);
             }
         }
